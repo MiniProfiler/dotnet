c0188293a1c7b81f5cecb3fe5aa358432110e4df
jdixon <none@none>
2011-07-11 21:22:11 -0400

    add sql server compact db and stub for testing SqlServerStorage

---
+++ MvcMiniProfiler.Tests/Storage/SqlServerStorageTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Threading;
+
+using NUnit.Framework;
+
+namespace MvcMiniProfiler.Tests.Storage
+{
+    [TestClass]
+    public class SqlServerStorageTest : BaseTest
+    {
+
+    }
+}
