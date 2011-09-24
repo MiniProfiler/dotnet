01188aeef3f5d5da0fe167a9a26e69d8b7fec694
Mark Young <mark@developer.geek.nz>
2011-09-24 19:28:35 +1200

    Issues 100 - Added MiniProfilerEF.Initialize_EF42 to workaround EF bug
    Signed-off-by: Mark Young <mark@developer.geek.nz>

---
+++ MvcMiniProfiler.EntityFramework/EFProfiledOdbcProviderFactory.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.SqlClient;
+using System.Data.Odbc;
+
+namespace MvcMiniProfiler.Data
+{
+    /// <summary>
+    /// Specific implementation of EFProfiledDbProviderFactory&lt;OdbcFactory&gt; to enable profiling
+    /// </summary>
+    public class EFProfiledOdbcProviderFactory
+        : EFProfiledDbProviderFactory<OdbcFactory>
+    {
+        private EFProfiledOdbcProviderFactory()
+        { }
+
+        /// <summary>
+        /// Every provider factory must have an Instance public field
+        /// </summary>
+        public static new EFProfiledOdbcProviderFactory Instance = new EFProfiledOdbcProviderFactory();
+
+    }
+}
