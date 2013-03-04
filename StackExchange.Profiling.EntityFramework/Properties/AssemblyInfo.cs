5fec6b6a5170fb335ede4ce95d429c79cd0829dd
Jarrod Dixon <jarrod.m.dixon@gmail.com>
2013-03-03 23:38:02 -0500

    update .net assembly versions and nuget package configs for a release

--- StackExchange.Profiling.EntityFramework/Properties/AssemblyInfo.cs
+++ StackExchange.Profiling.EntityFramework/Properties/AssemblyInfo.cs
@@ -12,5 +12,4 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 [assembly: Guid("cd063086-1c8a-4056-8525-85a831f88ee6")]
 
-[assembly: AssemblyVersion("2.0.4.0")]
-[assembly: AssemblyFileVersion("2.0.4.0")]
+[assembly: AssemblyVersion("2.1.0")]
