e8f7db0d54001371d60f723d4054191b0912c248
doconnell <doconnell565@gmail.com>
2013-07-19 21:37:38 -0500

    Fixes bug in SqlTimingParameter.GetHashCode
    Calling SqlTimingParameter's GetHashCode implementation when Value is
    null throws a NullReferenceException.

--- StackExchange.Profiling/SqlTimingParameter.cs
+++ StackExchange.Profiling/SqlTimingParameter.cs
@@ -57,7 +57,12 @@ namespace StackExchange.Profiling
         /// </summary>
         public override int GetHashCode()
         {
-            return ParentSqlTimingId.GetHashCode() ^ Name.GetHashCode() ^ Value.GetHashCode();
+            int hashcode = ParentSqlTimingId.GetHashCode() ^ Name.GetHashCode();
+            
+            if (Value != null)
+                hashcode ^= Value.GetHashCode();
+
+            return hashcode;
         }
 
         /// <summary>
