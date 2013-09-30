f790e3630f5f1c562df66b397efb05df532ca592
Jarrod Dixon <jarrod.m.dixon@gmail.com>
2013-09-30 01:51:58 -0400

    update nuget specs; add nuspec for StackExchange.Profiling.Mvc

--- StackExchange.Profiling.Mvc/Properties/AssemblyInfo.cs
+++ StackExchange.Profiling.Mvc/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("StackExchange.Profiling.Mvc")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A simple but effective mini-profiler for ASP.NET MVC - http://miniprofiler.com")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Stack Exchange")]
 [assembly: AssemblyProduct("StackExchange.Profiling.Mvc")]
 [assembly: AssemblyCopyright("Copyright ©  2013")]
 [assembly: AssemblyTrademark("")]
@@ -22,15 +22,4 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("44f8ac97-8e2c-4dba-adae-d971fb086a19")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("3.0.0")]
