124f860deca648e4ca2ed171e634ceccca53ad32
yellis <yaakov@ellisweb.net>
2013-10-05 22:35:40 +0300

    Add new render positions for bottom left/right

--- StackExchange.Profiling/RenderPosition.cs
+++ StackExchange.Profiling/RenderPosition.cs
@@ -1,18 +1,28 @@
 ﻿namespace StackExchange.Profiling
 {
     /// <summary>
-    /// Dictates on which side of the page the profiler popup button is displayed; defaults to left.
+    /// Dictates on which side of the page the profiler popup button is displayed; defaults to top left.
     /// </summary>
     public enum RenderPosition
     {
         /// <summary>
-        /// Profiler popup button is displayed on the left.
+        /// Profiler popup button is displayed on the top left.
         /// </summary>
         Left = 0,
 
         /// <summary>
-        /// Profiler popup button is displayed on the right.
+        /// Profiler popup button is displayed on the top right.
         /// </summary>
-        Right = 1
+        Right = 1,
+
+        /// <summary>
+        /// Profiler popup button is displayed on the bottom left.
+        /// </summary>
+        BottomLeft = 2,
+
+        /// <summary>
+        /// Profiler popup button is displayed on the bottom right.
+        /// </summary>
+        BottomRight = 3
     }
 }
