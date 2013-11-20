9a006aea6f57c577473e1c1e5fa100f9b2a584ff
Kamran Ayub <kamran.ayub@gmail.com>
2013-11-20 15:54:03 -0600

    Update solution to VS2013
    Updated gitignore to ignore packages directory

--- Sample.Mvc/EfModelFirst/EfModelFirstContext.Designer.cs
+++ Sample.Mvc/EfModelFirst/EfModelFirstContext.Designer.cs
@@ -1,4 +1,10 @@
-﻿// Default code generation is disabled for model 'C:\Code\github\MiniProfiler\dotnet\Sample.Mvc\EfModelFirst\EfModelFirstContext.edmx'. 
-// To enable default code generation, change the value of the 'Code Generation Strategy' designer
-// property to an alternate value. This property is available in the Properties Window when the model is
-// open in the designer.
+﻿// T4 code generation is enabled for model 'D:\Development\Contrib\dotnet\Sample.Mvc\EfModelFirst\EfModelFirstContext.edmx'. 
+// To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
+// property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
+// is open in the designer.
+
+// If no context and entity classes have been generated, it may be because you created an empty model but
+// have not yet chosen which version of Entity Framework to use. To generate a context class and entity
+// classes for your model, open the model in the designer, right-click on the designer surface, and
+// select 'Update Model from Database...', 'Generate Database from Model...', or 'Add Code Generation
+// Item...'.
