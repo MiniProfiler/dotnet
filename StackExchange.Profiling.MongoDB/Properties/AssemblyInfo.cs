c46374dd069fbf4efb17f956ae29ddc6d3f530e7
Yaakov Ellis <yaakov@ellisweb.net>
2014-05-27 11:40:31 +0300

    MiniProfiler.MongoDb nuspec and AseemblyInfo

--- StackExchange.Profiling.MongoDB/Properties/AssemblyInfo.cs
+++ StackExchange.Profiling.MongoDB/Properties/AssemblyInfo.cs
@@ -1,36 +1,15 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
 [assembly: AssemblyTitle("StackExchange.Profiling.MongoDB")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("MongoDb extensions for StackExchange.Profiling - http://miniprofiler.com")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Stack Exchange")]
 [assembly: AssemblyProduct("StackExchange.Profiling.MongoDB")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyCopyright("Copyright © 2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("a502ee5c-a969-4cdf-b243-2c54c0e94c27")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("3.0.11")]
+[assembly: AssemblyInformationalVersion("3.0.11")]
