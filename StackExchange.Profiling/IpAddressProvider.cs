8607fe02d9261827f2a721563891ef10948830af
Adam Nelson <anelson@bauer-media.com.au>
2014-05-29 18:36:32 +1000

    Combine both the REMOTE_ADDR header (which is the connecting device's IP address), plus the HTTP_X_FORWARDED_FOR header if present (which is set by some proxy servers and load balancers). This allows us to have a unique per-user view, even when behind a proxy or load balancer.

--- StackExchange.Profiling/IpAddressProvider.cs
+++ StackExchange.Profiling/IpAddressProvider.cs
@@ -9,10 +9,15 @@ namespace StackExchange.Profiling
     {
         /// <summary>
         /// Returns the paramter HttpRequest's client ip address.
+        /// We combine both the REMOTE_ADDR header (which is the connecting device's IP address),
+        /// plus the HTTP_X_FORWARDED_FOR header if present (which is set by some proxy
+        /// servers and load balancers). This allows us to have a unique per-user view, even
+        /// when behind a proxy or load balancer.
         /// </summary>
         public string GetUser(HttpRequest request)
         {
-            return request.ServerVariables["REMOTE_ADDR"] ?? "";
+            return string.Format("{0}, {1}", request.ServerVariables["REMOTE_ADDR"] ?? "",
+                                             request.ServerVariables["HTTP_X_FORWARDED_FOR"] ?? "");
         }
     }
 }
