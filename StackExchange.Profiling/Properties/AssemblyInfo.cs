3b8af6a85b356997d5d03c25cf31f648aedc4a85
Yaakov Ellis <yaakov@ellisweb.net>
2014-06-15 14:46:00 +0300

    Fix assembly versioning (#77)

--- StackExchange.Profiling/Properties/AssemblyInfo.cs
+++ StackExchange.Profiling/Properties/AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 [assembly: Guid("f41f8a8f-55ef-44ac-beaf-d6fa475939a7")]
 
-[assembly: AssemblyVersion("3.0.11")]
-[assembly: AssemblyInformationalVersion("3.0.11")]
+[assembly: AssemblyVersion("3.1")]
+[assembly: AssemblyInformationalVersion("3.1")]
 
 [assembly: InternalsVisibleTo("StackExchange.Profiling.Tests, PublicKey=00240000048000009400000006020000002400005253413100040000010001001172d8b313c5125b3503ab4656bc2c04305498b2c75560f2e4fdc70e22fd098b9fdce619c318b5fb92670406d2122c9912ee718cb24544f69b7a92a825bd6e439bf1b51cf8e32be536954881c067a81e3da26ca348f7a17de7dfc07ae6df14ce132a7cfcb6d5f53f939bc9f623ec0e5ff9edab439c04a9b8bf8c110ebe8908be")]
