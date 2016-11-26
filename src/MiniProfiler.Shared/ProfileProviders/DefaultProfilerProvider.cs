7170835ea9d44371d0a7fb3ee357dd11c3bed4a3
Nick Craver <nrcraver@gmail.com>
2016-11-25 23:10:35 -0500

    First bits of async support

---
+++ src/MiniProfiler.Shared/ProfileProviders/DefaultProfilerProvider.cs
@@ -0,0 +1,86 @@
+﻿#if NET45
+using System;
+using System.Runtime.Remoting.Messaging;
+using System.Web;
+#else
+using System.Threading;
+#endif
+
+namespace StackExchange.Profiling
+{
+    /// <summary>
+    /// Default profile provider, gracefully handles async transitions.
+    /// </summary>
+    public class DefaultProfilerProvider : BaseProfilerProvider
+    {
+#if NET45
+        const string ContextKey = ":miniprofiler:";
+
+        private MiniProfiler Profiler
+        {
+            get
+            {
+                if (HttpContext.Current != null)
+                {
+                    return HttpContext.Current?.Items[ContextKey] as MiniProfiler;
+                }
+                else
+                {
+                    return CallContext.LogicalGetData(ContextKey) as MiniProfiler;
+                }
+            }
+            set
+            {
+                if (HttpContext.Current != null)
+                {
+                    HttpContext.Current.Items[ContextKey] = value;
+                }
+                else
+                {
+                    CallContext.LogicalSetData(ContextKey, value);
+                }
+            }
+        }
+#else
+        private AsyncLocal<MiniProfiler> _profiler = new AsyncLocal<MiniProfiler>();
+
+        private MiniProfiler Profiler
+        {
+            get { return _profiler.Value; }
+            set { _profiler.Value = value; }
+        }
+#endif
+
+        /// <summary>
+        /// The name says it all.
+        /// </summary>
+        public override MiniProfiler GetCurrentProfiler() => Profiler;
+
+        /// <summary>
+        /// Starts a new profiling session.
+        /// </summary>
+        public override MiniProfiler Start(string sessionName = null)
+        {
+#if NET45
+            Profiler = new MiniProfiler(sessionName ?? AppDomain.CurrentDomain.FriendlyName) { IsActive = true };
+#else       // TODO: Revisit with .NET Standard 2.0
+            Profiler = new MiniProfiler(sessionName ?? "MiniProfiler") { IsActive = true };
+#endif
+            SetProfilerActive(Profiler);
+
+            return Profiler;
+        }
+
+        /// <summary>
+        /// Stops the current profiling session.
+        /// </summary>
+        public override void Stop(bool discardResults)
+        {
+            Profiler?.StopImpl();
+            if (discardResults)
+            {
+                Profiler = null;
+            }
+        }
+    }
+}
