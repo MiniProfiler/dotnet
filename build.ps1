be3b040204b40f14e382c73d12fbe83754426d5b
Nick Craver <nrcraver@gmail.com>
2016-11-30 23:21:58 -0500

    .\build.ps1, a first rudimentary CLI sanity build Note: there are multiple-import warning for NuGet.Build.Tasks.Pack.targets - not sure if this is my goof or a Preview 3 issue, need to investigate.

---
+++ build.ps1
@@ -0,0 +1,28 @@
+$projectsToBuild =
+    'MiniProfiler.Shared',
+    'MiniProfiler',
+    #'MiniProfiler.EntityFramework',
+    #'MiniProfiler.MongoDB',
+    'MiniProfiler.Mvc',
+    'MiniProfiler.Providers.RavenDB',
+    'MiniProfiler.Providers.SqlServer',
+    'MiniProfiler.Providers.SqlServerCe'
+
+Write-Host "Hello and welcome to our elaborate build!"
+Write-Host "Just kidding, this is a sanity check at the moment, it'll get more detailed."
+
+foreach ($project in $projectsToBuild) {
+    Write-Host "Working on $project`:" -ForegroundColor "Magenta"
+    Push-Location ".\src\$project"
+
+    Write-Host "Restoring $project..." -ForegroundColor "Magenta"
+    dotnet restore
+    Write-Host "Building $project..." -ForegroundColor "Magenta"
+    dotnet build
+    Write-Host "Packing $project..." -ForegroundColor "Magenta"
+    dotnet pack
+
+    Write-Host "Done."
+    wRite-Host ""
+    Pop-Location
+}
