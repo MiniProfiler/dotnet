309ec37f971c69b41e6c89f9aaf72a5635e6b7b6
Nick Craver <nrcraver@gmail.com>
2016-12-22 11:21:51 -0500

    Revert "Remove Newtonsoft Dependency in favor of SimpleJson"
    This reverts commit 5bfd9db2b01451d539b3d578170445e4dff3cfa0.

--- src/MiniProfiler.Shared/Helpers/ExtensionMethods.cs
+++ src/MiniProfiler.Shared/Helpers/ExtensionMethods.cs
@@ -1,4 +1,9 @@
 ﻿using System.Text.RegularExpressions;
+#if NET45
+using System.Web.Script.Serialization;
+#else
+using Newtonsoft.Json;
+#endif
 
 namespace StackExchange.Profiling.Helpers
 {
@@ -41,7 +46,12 @@ namespace StackExchange.Profiling.Helpers
         /// <returns>the resulting JSON object as a string</returns>
         public static string ToJson(this object o)
         {
-            return o != null ? SimpleJson.SimpleJson.SerializeObject(o) : null;
+            return o != null
+#if NET45
+                ? new JavaScriptSerializer() { MaxJsonLength = int.MaxValue }.Serialize(o) : null;
+#else
+                ? JsonConvert.SerializeObject(o) : null;
+#endif
         }
 
         /// <summary>
@@ -51,7 +61,12 @@ namespace StackExchange.Profiling.Helpers
         /// <returns>The object resulting from the given string</returns>
         public static T FromJson<T>(this string s) where T : class
         {
-            return s.HasValue() ? SimpleJson.SimpleJson.DeserializeObject<T>(s) : null;
+            return s.HasValue()
+#if NET45
+                ? new JavaScriptSerializer().Deserialize<T>(s) : null;
+#else
+                ? JsonConvert.DeserializeObject<T>(s) : null;
+#endif
         }
     }
 }
--- src/MiniProfiler.Shared/MiniProfiler.cs
+++ src/MiniProfiler.Shared/MiniProfiler.cs
@@ -4,6 +4,12 @@ using System.IO;
 using System.Runtime.Serialization;
 using StackExchange.Profiling.Helpers;
 using StackExchange.Profiling.Storage;
+#if NET45
+using System.Web.Script.Serialization;
+#else
+// TODO: Factor these extensions out? That'd be more breaks...
+using Newtonsoft.Json;
+#endif
 
 namespace StackExchange.Profiling
 {
@@ -258,7 +264,12 @@ namespace StackExchange.Profiling
         /// </summary>
         public static string ToJson(MiniProfiler profiler)
         {
-            return profiler != null ? SimpleJson.SimpleJson.SerializeObject(profiler) : null;
+            return profiler != null
+#if NET45
+                ? GetJsonSerializer().Serialize(profiler) : null;
+#else
+                ? JsonConvert.SerializeObject(profiler) : null;
+#endif
         }
 
         /// <summary>
@@ -266,8 +277,20 @@ namespace StackExchange.Profiling
         /// </summary>
         public static MiniProfiler FromJson(string json)
         {
-            return json.HasValue() ? SimpleJson.SimpleJson.DeserializeObject<MiniProfiler>(json) : null;
+            return json.HasValue()
+#if NET45
+                ? GetJsonSerializer().Deserialize<MiniProfiler>(json) : null;
+#else
+                ? JsonConvert.DeserializeObject<MiniProfiler>(json) : null;
+#endif
         }
+
+#if NET45
+        private static JavaScriptSerializer GetJsonSerializer()
+        {
+            return new JavaScriptSerializer { MaxJsonLength = Settings.MaxJsonResponseSize };   
+        }
+#endif
         
         /// <summary>
         /// Returns the <see cref="Root"/>'s <see cref="Timing.Name"/> and <see cref="DurationMilliseconds"/> this profiler recorded.
