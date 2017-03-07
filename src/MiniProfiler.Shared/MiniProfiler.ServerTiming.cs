4315a433712aef6904d206db7a5f8df253264c39
Nick Craver <nrcraver@gmail.com>
2017-03-07 14:38:29 -0500

    Add Server-Timing generation in
    v1 - Includes custom timings but not steps...we could add those as keys
    possibly (optional param?)

---
+++ src/MiniProfiler.Shared/MiniProfiler.ServerTiming.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using System.Text;
+
+namespace StackExchange.Profiling
+{
+    public partial class MiniProfiler
+    {
+        /// <summary>
+        /// Gets the Server-Timing header for this profiler, summarizing where time was spent for the browser.
+        /// Example output: sql=0.009; "sql", redis=0.005; "redis", aspnet=0.020; "ASP.NET"
+        /// </summary>
+        /// <returns>A string, the value to put in a Server-Timing header.</returns>
+        public string GetServerTimingHeader()
+        {
+            var total = DurationMilliseconds;
+            var summary = new Dictionary<string, decimal>();
+            foreach (var t in GetTimingHierarchy())
+            {
+                if (t.CustomTimings == null)
+                {
+                    continue;
+                }
+                foreach (var ct in t.CustomTimings)
+                {
+                    if (ct.Value?.Count > 0)
+                    {
+                        decimal ctTotal = 0;
+                        for (var i = 0; i < ct.Value.Count; i++)
+                        {
+                            ctTotal += ct.Value[i]?.DurationMilliseconds ?? 0;
+                        }
+                        summary[ct.Key] = (summary.TryGetValue(ct.Key, out decimal cur) ? cur : 0) + ctTotal;
+                    }
+                }
+            }
+
+            var sb = new StringBuilder();
+            foreach (var category in summary)
+            {
+                sb.Append(category.Key).Append('=').Append(category.Value / 1000)
+                  .Append("; \"").Append(category.Key).Append("\",");
+                total -= category.Value;
+            }
+            sb.Append("aspnet=").Append(total <= 0 ? 0 : total / 1000).Append("; \"ASP.NET\"");
+
+            // Server-Timing: sql=0.009; "sql", redis=0.005; "redis", aspnet=0.020; "ASP.NET"
+            return sb.ToString();
+        }
+    }
+}
