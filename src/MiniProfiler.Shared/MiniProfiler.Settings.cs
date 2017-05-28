2b28741f194bd9b4479e9827dddc85da6802792a
Nick Craver <nrcraver@gmail.com>
2017-05-28 16:16:35 -0400

    Default storage in NetFx to MemoryCacheStorage

--- src/MiniProfiler.Shared/MiniProfiler.Settings.cs
+++ src/MiniProfiler.Shared/MiniProfiler.Settings.cs
@@ -14,35 +14,6 @@ namespace StackExchange.Profiling
         /// </summary>
         public static class Settings
         {
-            private static readonly HashSet<string> assembliesToExclude = new HashSet<string>
-                {
-                    // our assembly
-                    "MiniProfiler",
-                    "MiniProfiler.Mvc",
-                    "MiniProfiler.Shared",
-                    "MiniProfiler.AspNetCore",
-                    "MiniProfiler.AspNetCore.Mvc",
-                    // reflection emit
-                    "Anonymously Hosted DynamicMethods Assembly",
-                    // the man
-                    "System.Core",
-                    "System.Data",
-                    "System.Data.Linq",
-                    "System.Web",
-                    "System.Web.Mvc",
-                    "mscorlib",
-                };
-            private static readonly HashSet<string> typesToExclude = new HashSet<string>
-                {
-                    // while we like our Dapper friend, we don't want to see him all the time
-                    "SqlMapper"
-                };
-            private static readonly HashSet<string> methodsToExclude = new HashSet<string>
-                {
-                    "lambda_method",
-                    ".ctor"
-                };
-
             /// <summary>
             /// The path under which ALL routes are registered in, defaults to the application root.  For example, "~/myDirectory/" would yield
             /// "/myDirectory/includes.js" rather than just "/mini-profiler-resources/includes.js"
@@ -64,37 +35,62 @@ namespace StackExchange.Profiling
             /// Assemblies to exclude from the stack trace report.
             /// Add to this using the <see cref="ExcludeAssembly"/> method.
             /// </summary>
-            public static HashSet<string> AssembliesToExclude => assembliesToExclude;
+            public static HashSet<string> AssembliesToExclude { get; } = new HashSet<string>
+                {
+                    // our assembly
+                    "MiniProfiler",
+                    "MiniProfiler.Mvc",
+                    "MiniProfiler.Shared",
+                    "MiniProfiler.AspNetCore",
+                    "MiniProfiler.AspNetCore.Mvc",
+                    // reflection emit
+                    "Anonymously Hosted DynamicMethods Assembly",
+                    // the man
+                    "System.Core",
+                    "System.Data",
+                    "System.Data.Linq",
+                    "System.Web",
+                    "System.Web.Mvc",
+                    "mscorlib",
+                };
 
             /// <summary>
             /// Types to exclude from the stack trace report.
             /// Add to this using the <see cref="ExcludeType"/> method.
             /// </summary>
-            public static HashSet<string> TypesToExclude => typesToExclude;
+            public static HashSet<string> TypesToExclude { get; } = new HashSet<string>
+                {
+                    // while we like our Dapper friend, we don't want to see him all the time
+                    "SqlMapper"
+                };
 
             /// <summary>
             /// Methods to exclude from the stack trace report.
             /// Add to this using the <see cref="ExcludeMethod"/> method.
             /// </summary>
-            public static HashSet<string> MethodsToExclude => methodsToExclude;
+            public static HashSet<string> MethodsToExclude { get; } = new HashSet<string>
+                {
+                    "lambda_method",
+                    ".ctor"
+                };
 
             /// <summary>
             /// Excludes the specified assembly from the stack trace output.
             /// </summary>
             /// <param name="assemblyName">The short name of the assembly. AssemblyName.Name</param>
-            public static void ExcludeAssembly(string assemblyName) => assembliesToExclude.Add(assemblyName);
+            public static void ExcludeAssembly(string assemblyName) => AssembliesToExclude.Add(assemblyName);
 
             /// <summary>
             /// Excludes the specified type from the stack trace output.
             /// </summary>
             /// <param name="typeToExclude">The System.Type name to exclude</param>
-            public static void ExcludeType(string typeToExclude) => typesToExclude.Add(typeToExclude);
+            public static void ExcludeType(string typeToExclude) => TypesToExclude.Add(typeToExclude);
 
             /// <summary>
             /// Excludes the specified method name from the stack trace output.
             /// </summary>
             /// <param name="methodName">The name of the method</param>
-            public static void ExcludeMethod(string methodName) => methodsToExclude.Add(methodName);
+            public static void ExcludeMethod(string methodName) => MethodsToExclude.Add(methodName);
 
             /// <summary>
             /// The maximum number of unviewed profiler sessions (set this low cause we don't want to blow up headers)
@@ -157,10 +153,12 @@ namespace StackExchange.Profiling
             /// will be collected, possibly improving profiler performance.
             /// </summary>
             public static bool ExcludeStackTraceSnippetFromCustomTimings { get; set; } = false;
-            
+
             /// <summary>
             /// Understands how to save and load MiniProfilers. Used for caching between when
             /// a profiling session ends and results can be fetched to the client, and for showing shared, full-page results.
+            /// On netstandard, defaults to <see cref="NullStorage"/>.
+            /// On netfx, defaults to <see cref="MemoryCacheStorage"/> with a duration of 30 minutes.
             /// </summary>
             /// <remarks>
             /// The normal profiling session life-cycle is as follows:
@@ -173,7 +171,12 @@ namespace StackExchange.Profiling
             /// 7) page is displayed and profiling results are ajax-fetched down, pulling cached results from 
             ///    <see cref="Storage"/>'s implementation of <see cref="IAsyncStorage.Load"/>
             /// </remarks>
-            public static IAsyncStorage Storage { get; set; } = new NullStorage();
+            public static IAsyncStorage Storage { get; set; } =
+#if NETSTANDARD1_5 || NETSTANDARD2_0
+                new NullStorage();
+#else
+                new MemoryCacheStorage(TimeSpan.FromMinutes(30));
+#endif
 
             /// <summary>
             /// The formatter applied to any SQL before being set in a <see cref="CustomTiming.CommandString"/>.
