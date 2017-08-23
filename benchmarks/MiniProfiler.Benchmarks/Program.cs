87567846753ccf582a155e4a6add78fe2bd2ec06
Nick Craver <craver@stackoverflow.com>
2017-08-23 10:34:10 -0400

    Quick toggle for dotMemory style profiling

--- benchmarks/MiniProfiler.Benchmarks/Program.cs
+++ benchmarks/MiniProfiler.Benchmarks/Program.cs
@@ -7,6 +7,11 @@ namespace Benchmarks
     {
         public static void Main(string[] args)
         {
+            //var creation = new Benchmarks.CreationBenchmarks();
+            //while (true)
+            //{
+            //    creation.StartStopProfiler();
+            //}
             BenchmarkSwitcher.FromAssembly(typeof(Program).GetTypeInfo().Assembly).Run(args);
         }
     }
