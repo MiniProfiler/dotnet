b2c37e494c26ab3d01ec2c7a9512f75c280eb7c9
Nick Craver <nrcraver@gmail.com>
2017-09-17 10:47:25 -0400

    EntityFrameworkCore: remove support before netstandard2.0
    The facts are that the output of events to listen to changed to drastically in 2.0 on the EntityFramework side that trying to analyze across package versions is just crazy. Given the timeline of MiniProfiler release (after 2.0 is RTM, which is already the case), it makes sense to just support EF Core 2.0 and up.
    
    This should resolve the issues users are hitting in #209.

--- src/MiniProfiler.EntityFrameworkCore/RelationalDiagnosticListener.cs
+++ src/MiniProfiler.EntityFrameworkCore/RelationalDiagnosticListener.cs
@@ -1,14 +1,10 @@
-﻿using Microsoft.EntityFrameworkCore.Storage;
+﻿using Microsoft.EntityFrameworkCore.Diagnostics;
+using Microsoft.EntityFrameworkCore.Storage;
 using StackExchange.Profiling.Internal;
 using System;
 using System.Collections.Concurrent;
-#if NETSTANDARD2_0
-using Microsoft.EntityFrameworkCore.Diagnostics;
 using System.Collections.Generic;
-#else
-using Microsoft.Extensions.DiagnosticAdapter;
-using System.Data.Common;
-#endif
+using System.Diagnostics;
 
 namespace StackExchange.Profiling.Data
 {
@@ -31,7 +27,6 @@ namespace StackExchange.Profiling.Data
             _opening = new ConcurrentDictionary<Guid, CustomTiming>(),
             _closing = new ConcurrentDictionary<Guid, CustomTiming>();
 
-#if NETSTANDARD2_0
         // See https://github.com/aspnet/EntityFramework/issues/8007
         private readonly ConcurrentDictionary<Guid, CustomTiming>
             _readers = new ConcurrentDictionary<Guid, CustomTiming>();
@@ -40,13 +35,11 @@ namespace StackExchange.Profiling.Data
         /// Notifies the observer that the provider has finished sending push-based notifications.
         /// </summary>
         public void OnCompleted() { }
-#pragma warning disable RCS1163 // Unused parameter.
         /// <summary>
         /// Notifies the observer that the provider has experienced an error condition.
         /// </summary>
         /// <param name="error">An object that provides additional information about the error.</param>
-        public void OnError(Exception error) { }
-#pragma warning restore RCS1163 // Unused parameter.
+        public void OnError(Exception error) => Trace.WriteLine(error);
 
         /// <summary>
         /// Provides the observer with new data.
@@ -150,168 +143,6 @@ namespace StackExchange.Profiling.Data
                 }
             }
         }
-#else
-        private readonly ConcurrentDictionary<DbDataReader, CustomTiming>
-            _readers = new ConcurrentDictionary<DbDataReader, CustomTiming>();
-
-        /// <summary>
-        /// Handles BeforeExecuteCommand events. Fired just before a command is started.
-        /// </summary>
-        /// <param name="command">The <see cref="DbCommand"/> being executed.</param>
-        /// <param name="executeMethod">The execution method of the command, e.g. "ExecuteNonQuery"</param>
-        /// <param name="instanceId">The <see cref="Guid"/> identifier for this command.</param>
-        /// <param name="async">Whether this command was executed asynchronously.</param>
-        [DiagnosticName("Microsoft.EntityFrameworkCore.BeforeExecuteCommand")]
-        public void OnBeforeExecuteCommand(DbCommand command, string executeMethod, Guid instanceId, bool async)
-        {
-            // Available: Guid connectionId, DbCommand command, string executeMethod, Guid instanceId, long startTimestamp, bool async
-            var timing = command.GetTiming(executeMethod + (async ? " (Async)" : null), MiniProfiler.Current);
-            if (timing != null)
-            {
-                _commands[instanceId] = timing;
-            }
-        }
-
-        /// <summary>
-        /// Handles AfterExecuteCommand events. Fired just after a command finishes.
-        /// </summary>
-        /// <param name="methodResult">The rest of the execution, e.g. the <see cref="RelationalDataReader"/>.</param>
-        /// <param name="instanceId">The <see cref="Guid"/> identifier for this command.</param>
-        [DiagnosticName("Microsoft.EntityFrameworkCore.AfterExecuteCommand")]
-        public void OnAfterExecuteCommand(object methodResult, Guid instanceId)
-        {
-            // Available: Guid connectionId, DbCommand command, string executeMethod, object methodResult, Guid instanceId, long startTimestamp, long currentTimestamp, bool async
-            if (!_commands.TryRemove(instanceId, out var current))
-            {
-                return;
-            }
-
-            // A completion for a DataReader only means we *started* getting data back, not finished.
-            if (methodResult is RelationalDataReader reader)
-            {
-                // TODO: Switch to Guid in 2.0
-                _readers[reader.DbDataReader] = current;
-                current.FirstFetchCompleted();
-            }
-            else
-            {
-                current.Stop();
-            }
-        }
-
-        /// <summary>
-        /// Handles CommandExecutionError events. Fired when a command goes boom during execution.
-        /// </summary>
-        /// <param name="instanceId">The <see cref="Guid"/> identifier for the command that errored.</param>
-        [DiagnosticName("Microsoft.EntityFrameworkCore.CommandExecutionError")]
-        public void OnCommandExecutionError(Guid instanceId)
-        {
-            // Available: Guid connectionId, DbCommand command, string executeMethod, Guid instanceId, long startTimestamp, long currentTimestamp, Exception exception, bool async
-            if (_commands.TryRemove(instanceId, out var command))
-            {
-                command.Errored = true;
-                command.Stop();
-            }
-        }
-
-        /// <summary>
-        /// Handles DataReaderDisposing events. Fired when a <see cref="DbDataReader"/> is disposed.
-        /// Usually, this is when it finishes consuming the available data.
-        /// </summary>
-        /// <param name="dataReader">The <see cref="DbDataReader"/> that is being disposed.</param>
-        [DiagnosticName("Microsoft.EntityFrameworkCore.DataReaderDisposing")]
-        public void OnDataReaderDisposing(DbDataReader dataReader)
-        {
-            // Available: DbConnection connection, Guid connectionId, DbDataReader dataReader, int recordsAffected, long startTimestamp, long currentTimestamp
-            // TODO: Move to a Guid after https://github.com/aspnet/EntityFramework/issues/8007
-            if (_readers.TryRemove(dataReader, out var reader))
-            {
-                reader.Stop();
-            }
-        }
-
-        /// <summary>
-        /// Handles ConnectionOpening events.
-        /// </summary>
-        /// <param name="instanceId">The <see cref="Guid"/> identifier for this *specific open*, not the connection.</param>
-        /// <param name="async">Whether this connection is opening asynchronously.</param>
-        [DiagnosticName("Microsoft.EntityFrameworkCore.ConnectionOpening")]
-        public void OnConnectionOpening(Guid instanceId, bool async)
-        {
-            // Available: DbConnection connection, Guid connectionId, Guid instanceId, long startTimestamp, bool async
-            var timing = MiniProfiler.Current.CustomTiming("sql",
-                    async ? "Connection OpenAsync()" : "Connection Open()",
-                    async ? "OpenAsync" : "Open");
-            if (timing != null)
-            {
-                _opening[instanceId] = timing;
-            }
-        }
-
-        /// <summary>
-        /// Handles ConnectionOpened events.
-        /// </summary>
-        /// <param name="instanceId">The <see cref="Guid"/> identifier for this *specific open*, not the connection.</param>
-        [DiagnosticName("Microsoft.EntityFrameworkCore.ConnectionOpened")]
-        public void OnConnectionOpened(Guid instanceId)
-        {
-            // Available: DbConnection connection, Guid connectionId, Guid instanceId, long startTimestamp, long currentTimestamp, bool async
-            if (_opening.TryRemove(instanceId, out var openingTiming))
-            {
-                openingTiming.Stop();
-            }
-        }
-
-        /// <summary>
-        /// Handles ConnectionClosing events.
-        /// </summary>
-        /// <param name="instanceId">The <see cref="Guid"/> identifier for this *specific close*, not the connection.</param>
-        /// <param name="async">Whether this connection is closing asynchronously.</param>
-        [DiagnosticName("Microsoft.EntityFrameworkCore.ConnectionClosing")]
-        public void OnConnectionClosing(Guid instanceId, bool async)
-        {
-            // Available: DbConnection connection, Guid connectionId, Guid instanceId, long startTimestamp, bool async
-            var timing = MiniProfiler.Current.CustomTiming("sql",
-                    async ? "Connection CloseAsync()" : "Connection Close()",
-                    async ? "CloseAsync" : "Close");
-            if (timing != null)
-            {
-                _closing[instanceId] = timing;
-            }
-        }
-
-        /// <summary>
-        /// Handles ConnectionClosed events.
-        /// </summary>
-        /// <param name="instanceId">The <see cref="Guid"/> identifier for this *specific close*, not the connection.</param>
-        [DiagnosticName("Microsoft.EntityFrameworkCore.ConnectionClosed")]
-        public void OnConnectionClosed(Guid instanceId)
-        {
-            // Available: DbConnection connection, Guid connectionId, Guid instanceId, long startTimestamp, long currentTimestamp, bool async
-            if (_closing.TryRemove(instanceId, out var closingTiming))
-            {
-                closingTiming.Stop();
-            }
-        }
-
-        /// <summary>
-        /// Handles ConnectionError events. Fires when a connection goes boom while opening or closing.
-        /// </summary>
-        /// <param name="instanceId">The <see cref="Guid"/> identifier for this *specific open or close*, not the connection.</param>
-        [DiagnosticName("Microsoft.EntityFrameworkCore.ConnectionError")]
-        public void OnConnectionError(Guid instanceId)
-        {
-            // Available: DbConnection connection, Guid connectionId, Exception exception, Guid instanceId, long startTimestamp, long currentTimestamp, bool async
-            if (_opening.TryRemove(instanceId, out var openingTiming))
-            {
-                openingTiming.Errored = true;
-            }
-            if (_closing.TryRemove(instanceId, out var closingTiming))
-            {
-                closingTiming.Errored = true;
-            }
-        }
-#endif
 
         // Transactions - Not in yet
         //[DiagnosticName("Microsoft.EntityFrameworkCore.TransactionStarted")]
