8bac526f5c32943d1a03012d08a7d6e4341cca41
Nick Craver <nrcraver@gmail.com>
2018-02-11 11:32:19 -0500

    ASP.NET MVC Sample: add area testing

--- samples/Samples.AspNetCore/Startup.cs
+++ samples/Samples.AspNetCore/Startup.cs
@@ -92,6 +92,10 @@ namespace Samples.AspNetCore
             app.UseMvc(routes =>
             {
                 routes.MapRoute(
+                    name: "areaRoute",
+                    template: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
+
+                routes.MapRoute(
                     name: "default",
                     template: "{controller=Home}/{action=Index}/{id?}");
             });
