6feb645acca1765db72da4cb9ca265a3b5644e76
M3LiNdRu <rogercalaf@gmail.com>
2018-03-14 00:24:48 +0100

    Implemented MongoDbStorage test

---
+++ tests/MiniProfiler.Tests/Storage/MongoDbStorageTests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace StackExchange.Profiling.Tests.Storage
+{
+    public class MongoDbStorageTests : StorageBaseTest, IClassFixture<MySqlStorageFixture>
+    {
+        public MongoDbStorageTests(MySqlStorageFixture fixture, ITestOutputHelper output) : base(fixture, output)
+        {
+        }
+    }
+
+    public class MongoDbStorageFixture : StorageFixtureBase<MongoDbStorage>, IDisposable
+    {
+        public MongoDbStorageFixture()
+        {
+            Skip.IfNoConfig(nameof(TestConfig.Current.MongoDbConnectionString), TestConfig.Current.MongoDbConnectionString);
+
+            Storage = new MongoDbStorage(TestConfig.Current.MongoDbConnectionString);
+
+            try
+            {
+                Storage.CreateSchema();
+            }
+            catch (Exception e)
+            {
+                ShouldSkip = true;
+                SkipReason = e.Message;
+            }
+        }
+
+        public void Dispose()
+        {
+            if (!ShouldSkip)
+            {
+                Storage.DropSchema();
+            }
+        }
+    }
+}
