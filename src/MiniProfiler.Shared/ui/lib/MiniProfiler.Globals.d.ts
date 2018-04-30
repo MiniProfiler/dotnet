ff295c5b3bb7453f40533ccd29948b558129d900
Nick Craver <nrcraver@gmail.com>
2018-04-29 21:46:55 -0400

    TypeScript conversion: initial commit
    This commit lacks:
    - Syntax highlighting on custom timings
    - Gaps in custom timings
    
    In theory, everything else works. Note that includes.js is now ignored, as it's generated on build. TypeScript is compiled then bundled on save.

---
+++ src/MiniProfiler.Shared/ui/lib/MiniProfiler.Globals.d.ts
@@ -0,0 +1,47 @@
+﻿declare global {
+    interface Window {
+        PerformancePaintTiming: Function;
+        mPt: MiniProfilerProbeTiming;
+        chrome: {
+            loadTimes(): {
+                firstPaintTime: number;
+                firstPaintAfterLoadTime: number;
+            }
+        };
+        JSON: any;
+        jQuery: any;
+        angular: any;
+        axios: any;
+        xhr: any;
+        profiler: StackExchange.Profiling.Profiler;
+        MiniProfiler: StackExchange.Profiling.MiniProfiler;
+        WebForm_ExecuteCallback(object: any): void;
+    }
+
+    interface Document {
+        createStyleSheet(url: string): Function;
+    }
+
+    interface MiniProfilerProbeTiming {
+        start(name: string): void;
+        end(name: string): void;
+        results(): { [id: string]: { start: number; end: number; } };
+        flush(): void;
+    }
+
+    interface Request {
+        addEvents(object: any): void;
+    }
+
+    interface JQuery {
+        tmpl(object?: any): JQuery;
+    }
+
+    class MooTools { }
+
+    //function WebForm_ExecuteCallback(object: any): void;
+    // TODO: highlight.js
+    function prettyPrint(): void;
+}
+
+export { };
---
+++ src/MiniProfiler.Shared/ui/lib/node_modules/@types/highlight.js/README.md
@@ -0,0 +1,16 @@
+# Installation
+> `npm install --save @types/highlight.js`
+
+# Summary
+This package contains type definitions for highlight.js (https://github.com/isagalaev/highlight.js).
+
+# Details
+Files were exported from https://www.github.com/DefinitelyTyped/DefinitelyTyped/tree/master/types/highlight.js
+
+Additional Details
+ * Last updated: Tue, 28 Nov 2017 00:30:44 GMT
+ * Dependencies: none
+ * Global values: hljs
+
+# Credits
+These definitions were written by Niklas Mollenhauer <https://github.com/nikeee>, Jeremy Hull <https://github.com/sourrust>, Josh Goldberg <https://github.com/joshuakgoldberg>.
