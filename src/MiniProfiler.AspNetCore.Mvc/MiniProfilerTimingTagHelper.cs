7df2d463c4b99b08b05df5c1f4c70284039f7ef9
Delbert Matlock <delbert.matlock@gmail.com>
2018-06-29 07:31:51 -0400

    Tag helpers for client timing scripts.

---
+++ src/MiniProfiler.AspNetCore.Mvc/MiniProfilerTimingTagHelper.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace StackExchange.Profiling
+{
+    /// <summary>
+    /// Tag helper to render MiniProfiler client timing initalization in ASP.NET Core views, e.g.
+    /// &lt;mini-profiler-timing /&gt;
+    /// </summary>
+    [HtmlTargetElement("mini-profiler-timing", TagStructure = TagStructure.WithoutEndTag)]
+    public class MiniProfilerTimingTagHelper : TagHelper
+    {
+        internal const string ClientTimingKey = "MiniProfiler:ClientTimingScriptLoaded";
+
+        [ViewContext]
+        public ViewContext ViewContext { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = null;
+
+            if (MiniProfiler.Current == null || ViewContext.ViewData.ContainsKey(ClientTimingKey))
+                return;
+
+            output.Content.SetHtmlContent(ClientTimingHelper.InitScript);
+            ViewContext.ViewData[ClientTimingKey] = true;
+        }
+    }
+}
---
+++ src/MiniProfiler.AspNetCore.Mvc/ProfileScriptTagHelper.cs
@@ -0,0 +1,46 @@
+﻿using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace StackExchange.Profiling
+{
+    /// <summary>
+    /// Tag helper to profile script execution in ASP.NET Core views, e.g. 
+    /// &lt;profile-script name="My Step" /&gt;
+    /// ...script blocks...
+    /// &lt;/profile-script&gt;
+    /// </summary>
+    [HtmlTargetElement("profile-script", TagStructure = TagStructure.NormalOrSelfClosing)]
+    public class ProfileScriptTagHelper : TagHelper
+    {
+        [ViewContext]
+        public ViewContext ViewContext { get; set; }
+
+        /// <summary>
+        /// The name of this <see cref="MiniProfiler"/> step.
+        /// </summary>
+        [HtmlAttributeName("name")]
+        public string Name { get; set; }
+
+        public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = null;
+            output.Content = await output.GetChildContentAsync();
+
+            if (MiniProfiler.Current == null)
+                return;
+
+            var pre = new StringBuilder();
+            if (!ViewContext.ViewData.ContainsKey(MiniProfilerTimingTagHelper.ClientTimingKey))
+            {
+                pre.Append(ClientTimingHelper.InitScript);
+                ViewContext.ViewData[MiniProfilerTimingTagHelper.ClientTimingKey] = true;
+            }
+            pre.Append($"<script>mPt.start('{Name}')</script>");
+            output.PreContent.SetHtmlContent(pre.ToString());
+            output.PostContent.SetHtmlContent($"<script>mPt.end('{Name}')</script>");
+        }
+    }
+}
