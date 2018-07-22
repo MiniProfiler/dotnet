7f1155ead09fd21d57ba0540106f62b65248d3ae
Delbert Matlock <StuffOfInterest@users.noreply.github.com>
2018-07-22 10:41:02 -0400

    Tag helpers for client timing scripts (#304)
    Two tag helpers to initialize client timing scripts and to wrap a section of code for timing.  The initialization tag is optional as it will be pre-pended to the timing script if it hasn't already been loaded.  Also, the initialization script will only render once in case someone accidentally adds it twice.
    
    Code below is how I was testing the behavior for the helpers.
    
    ```
    <mini-profiler-timing />
    <mini-profiler-timing />
    <div></div>
    <profile name="Render JS delay">
    	<profile-script name="JS delay">
    		<script language="javascript">
    			alert("Wait for a second");
    		</script>
    	</profile-script>
    </profile>
    ```
    
    This partially addresses issues I mentioned in issue #302.

---
+++ src/MiniProfiler.AspNetCore.Mvc/ProfileScriptTagHelper.cs
@@ -0,0 +1,60 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace StackExchange.Profiling
+{
+    /// <summary>
+    /// Tag helper to profile script execution in ASP.NET Core views, e.g. 
+    /// &lt;profile-script name="My Step" /&gt;
+    /// ...script blocks...
+    /// &lt;/profile-script&gt;
+    /// Include as self closing to provide initialization only.
+    /// </summary>
+    [HtmlTargetElement("profile-script", TagStructure = TagStructure.NormalOrSelfClosing)]
+    public class ProfileScriptTagHelper : TagHelper
+    {
+        private const string ClientTimingKey = "MiniProfiler:ClientTiming";
+
+        /// <summary>
+        /// The <see cref="ViewContext"/> for this control, gets injected.
+        /// </summary>
+        [ViewContext]
+        [HtmlAttributeNotBound]
+        public ViewContext ViewContext { get; set; }
+
+        /// <summary>
+        /// The name of this <see cref="MiniProfiler"/> step.
+        /// </summary>
+        [HtmlAttributeName("name")]
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Renders the tag helper.
+        /// </summary>
+        /// <param name="context">The context we're rendering in.</param>
+        /// <param name="output">The output we're rendering to.</param>
+        /// <returns>The task to await.</returns>
+        public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = null;
+            output.Content = await output.GetChildContentAsync();
+
+            if (MiniProfiler.Current == null)
+                return;
+
+            if (!ViewContext.HttpContext.Items.ContainsKey(ClientTimingKey))
+            {
+                output.PreContent.AppendHtml(ClientTimingHelper.InitScript);
+                ViewContext.HttpContext.Items[ClientTimingKey] = true;
+            }
+
+            if (output.TagMode == TagMode.SelfClosing)
+                return;
+
+            output.PreContent.AppendHtml($"<script>mPt.start('{Name}')</script>");
+            output.PostContent.SetHtmlContent($"<script>mPt.end('{Name}')</script>");
+        }
+    }
+}
