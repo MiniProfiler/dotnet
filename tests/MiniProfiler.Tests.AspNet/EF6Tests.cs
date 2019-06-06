7166338fee27d0b1d85754769d5826708d9b5926
Nick Craver <craver@stackoverflow.com>
2019-06-05 20:59:44 -0400

    EF6: Add initial tests
    Getting this thing to even load is a royal PITA. But hey, now we can go test some scenarios.

---
+++ tests/MiniProfiler.Tests.AspNet/EF6Tests.cs
@@ -0,0 +1,30 @@
+﻿using System.Data.Common;
+using System.Data.Entity;
+using System.Data.Entity.Core.Common;
+using System.Data.Entity.Infrastructure;
+using StackExchange.Profiling.Data;
+using StackExchange.Profiling.EntityFramework6;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace StackExchange.Profiling.Tests
+{
+    public class EF6Tests : AspNetTest
+    {
+        public EF6Tests(ITestOutputHelper output) : base(output)
+        {
+            MiniProfilerEF6.Initialize();
+        }
+
+        [Fact]
+        public void ServicesCheck()
+        {
+            const string providerKey = "System.Data.SQLite";
+
+            Assert.IsType<EFProfiledDbProviderServices>(DbConfiguration.DependencyResolver.GetService(typeof(DbProviderServices), providerKey));
+            Assert.IsType<ProfiledDbProviderFactory>(DbConfiguration.DependencyResolver.GetService(typeof(DbProviderFactory), providerKey));
+            Assert.IsType<EFProfiledDbProviderFactoryResolver>(DbConfiguration.DependencyResolver.GetService(typeof(IDbProviderFactoryResolver), providerKey));
+            Assert.IsType<EFProfiledDbConnectionFactory>(DbConfiguration.DependencyResolver.GetService(typeof(IDbConnectionFactory), providerKey));
+        }
+    }
+}
