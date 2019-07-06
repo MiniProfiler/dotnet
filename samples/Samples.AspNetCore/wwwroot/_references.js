9ae0279c1a8d924cb7fcdcc2e1ec3fec5ca7dfbe
Nick Craver <craver@stackoverflow.com>
2019-07-05 21:19:33 -0400

    Cleanup ASP.NET Core sample

--- samples/Samples.AspNetCore/wwwroot/_references.js
+++ samples/Samples.AspNetCore/wwwroot/_references.js
@@ -2,5 +2,3 @@
 /// <reference path="js/site.js" />
 /// <reference path="lib/bootstrap/dist/js/bootstrap.js" />
 /// <reference path="lib/jquery/dist/jquery.js" />
-/// <reference path="lib/jquery-validation/dist/jquery.validate.js" />
-/// <reference path="lib/jquery-validation-unobtrusive/jquery.validate.unobtrusive.js" />
