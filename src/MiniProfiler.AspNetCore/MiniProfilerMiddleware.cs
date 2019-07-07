8d0f969d8dd39950955774ccfe00e960492d826e
Nick Craver <craver@stackoverflow.com>
2019-07-07 12:18:19 -0400

    Cleanup

--- src/MiniProfiler.AspNetCore/MiniProfilerMiddleware.cs
+++ src/MiniProfiler.AspNetCore/MiniProfilerMiddleware.cs
@@ -5,7 +5,6 @@ using Microsoft.Extensions.Options;
 using StackExchange.Profiling.Internal;
 using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
