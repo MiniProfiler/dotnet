71bbe9e548f18afe1df74df48c379625c8a94bbc
Nick Craver <craver@stackoverflow.com>
2020-04-18 14:49:38 -0400

    Update XML docs

--- src/MiniProfiler.AspNetCore/MiniProfilerOptions.cs
+++ src/MiniProfiler.AspNetCore/MiniProfilerOptions.cs
@@ -24,31 +24,29 @@ namespace StackExchange.Profiling
 
         /// <summary>
         /// A function that determines who can access the MiniProfiler results URL and list URL.  It should return true when
-        /// the request client has access to results, false for a 401 to be returned. HttpRequest parameter is the current request and
+        /// the request client has access to results, false for a 401 to be returned.
+        /// <see cref="HttpRequest"/> parameter is the current request and will not be null.
         /// </summary>
-        /// <remarks>
-        /// The HttpRequest parameter that will be passed into this function should never be null.
-        /// </remarks>
         public Func<HttpRequest, bool> ResultsAuthorize { get; set; }
 
         /// <summary>
         /// An async function that determines who can access the MiniProfiler results URL and list URL.  It should return true when
-        /// the request client has access to results, false for a 401 to be returned. HttpRequest parameter is the current request and
+        /// the request client has access to results, false for a 401 to be returned.
+        /// <see cref="HttpRequest"/> parameter is the current request and will not be null.
         /// </summary>
-        /// <remarks>
-        /// The HttpRequest parameter that will be passed into this function should never be null.
-        /// </remarks>
         public Func<HttpRequest, Task<bool>> ResultsAuthorizeAsync { get; set; }
 
         /// <summary>
         /// Special authorization function that is called for the list results (listing all the profiling sessions), 
         /// we also test for results authorize always. This must be set and return true, to enable the listing feature.
+        /// <see cref="HttpRequest"/> parameter is the current request and will not be null.
         /// </summary>
         public Func<HttpRequest, bool> ResultsListAuthorize { get; set; }
 
         /// <summary>
         /// Special async authorization function that is called for the list results (listing all the profiling sessions), 
         /// we also test for results authorize always. This must be set and return true, to enable the listing feature.
+        /// <see cref="HttpRequest"/> parameter is the current request and will not be null.
         /// </summary>
         public Func<HttpRequest, Task<bool>> ResultsListAuthorizeAsync { get; set; }
 
