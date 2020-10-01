2079f736f86c1e3309b8e05d7da4fae0648eea94
prafullthorve <33866010+prafullthorve@users.noreply.github.com>
2020-10-01 23:57:43 +0530

    Update Program.cs
    Removed the commented code. It is not necessary to keep the commented code as we can check in history.

--- benchmarks/MiniProfiler.Benchmarks/Program.cs
+++ benchmarks/MiniProfiler.Benchmarks/Program.cs
@@ -7,11 +7,6 @@ namespace Benchmarks
     {
         public static void Main(string[] args)
         {
-            //var creation = new Benchmarks.CreationBenchmarks();
-            //while (true)
-            //{
-            //    creation.StartStopProfiler();
-            //}
             BenchmarkSwitcher.FromAssembly(typeof(Program).GetTypeInfo().Assembly).Run(args);
         }
     }
