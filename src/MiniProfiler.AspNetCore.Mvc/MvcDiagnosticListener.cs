7c8c1923e3a0657467cd5aa70fb1078305f49df8
Nick Craver <nrcraver@gmail.com>
2021-11-07 09:23:48 -0500

    Tweak output and be explicit

--- src/MiniProfiler.AspNetCore.Mvc/MvcDiagnosticListener.cs
+++ src/MiniProfiler.AspNetCore.Mvc/MvcDiagnosticListener.cs
@@ -197,8 +197,12 @@ namespace StackExchange.Profiling.Data
                     AfterViewPageEventData data => Complete(data.Page),
 
                     // ViewComponent
-                    BeforeViewComponentEventData data => StartView(data.ViewComponentContext, "Component: " + data.ViewComponentContext.ViewComponentDescriptor.ShortName),
+                    BeforeViewComponentEventData data => StartView(data.ViewComponentContext, "Component (Invoke): " + data.ViewComponentContext.ViewComponentDescriptor.ShortName),
                     AfterViewComponentEventData data => Complete(data.ViewComponentContext),
+
+                    // Intentionally not registering to slim view wrapper due to noise, accounted for in View: above.
+                    //ViewComponentBeforeViewExecuteEventData data => StartView(data.ViewComponentContext, "Component (View): " + data.ViewComponentContext.ViewComponentDescriptor.ShortName),
+                    //ViewComponentAfterViewExecuteEventData data => Complete(data.ViewComponentContext),
 
                     // RazorPage Bits: https://github.com/dotnet/aspnetcore/blob/v3.0.0/src/Mvc/Mvc.RazorPages/src/Diagnostics/MvcDiagnostics.cs
                     // HandlerMethod
