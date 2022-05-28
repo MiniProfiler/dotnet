ce90f4033770b86831d50180bc9bca5a7fcbefb2
Ben Bryant <30258875+benbryant0@users.noreply.github.com>
2022-05-28 23:06:44 +0200

    Optimise StackTraceSnippet.Get (#605)
    * Remove RuntimeIdentifier
    
    This creates a self-contained executable, which BDN doesn't seem to support
    
    * Remove commented test code
    
    * Set up profiler per iteration
    
    If the benchmark is fast enough BDN will run enough operations per iteration that the accumulated CustomTimings will eat all memory.
    
    * Bench with stack trace vs without
    
    * Cache assembly & module names
    
    These are not (currently) cached in any meaningful way. Type & MethodInfo don't have the same problem.

--- benchmarks/MiniProfiler.Benchmarks/Benchmarks/CustomTimingBenchmarks.cs
+++ benchmarks/MiniProfiler.Benchmarks/Benchmarks/CustomTimingBenchmarks.cs
@@ -4,32 +4,35 @@ using StackExchange.Profiling;
 
 namespace Benchmarks
 {
-    [SimpleJob(RuntimeMoniker.Net472)]
-    [SimpleJob(RuntimeMoniker.Net50)]
+    [SimpleJob(RuntimeMoniker.Net472, invocationCount: 50_000)]
+    [SimpleJob(RuntimeMoniker.Net50, invocationCount: 50_000)]
     [Config(typeof(Configs.Memory))]
     public class CustomTimingBenchmarks
     {
         private MiniProfiler Profiler;
 
-        [GlobalSetup]
+        [Params(true, false)]
+        public bool IncludeStackTrace { get; set; }
+
+        [IterationSetup]
         public void SetupData()
         {
             Profiler = new MiniProfiler("Test", new MiniProfilerBenchmarkOptions());
         }
 
         [Benchmark(Description = "Creation of a standalone CustomTiming")]
-        public CustomTiming Creation() => new CustomTiming(Profiler, "Test");
+        public CustomTiming Creation() => new CustomTiming(Profiler, "Test", includeStackTrace: IncludeStackTrace);
 
         [Benchmark(Description = "Creation a CustomTiming via MiniProfiler")]
         public void AddingToMiniProfiler()
         {
-            Profiler.CustomTiming("Test", "MyCategory");
+            Profiler.CustomTiming("Test", "MyCategory", includeStackTrace: IncludeStackTrace);
         }
 
         [Benchmark(Description = "Using a CustomTiming with MiniProfiler")]
         public void UsingWithMiniProfiler()
         {
-            using (Profiler.CustomTiming("Test", "MyCategory"))
+            using (Profiler.CustomTiming("Test", "MyCategory", includeStackTrace: IncludeStackTrace))
             {
                 // Trigger the .Dispose()
             }
