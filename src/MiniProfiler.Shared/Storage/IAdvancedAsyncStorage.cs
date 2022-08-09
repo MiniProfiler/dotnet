830aee2efca9d0a1e92ceb8120856f513bfe12ab
Benjamin Grabkowitz <profet23@users.noreply.github.com>
2022-08-08 22:25:21 -0400

    Correcting wrong C# version.

--- src/MiniProfiler.Shared/Storage/IAdvancedAsyncStorage.cs
+++ src/MiniProfiler.Shared/Storage/IAdvancedAsyncStorage.cs
@@ -2,17 +2,18 @@
 using System.Collections.Generic;
 using System.Threading.Tasks;
 
-namespace StackExchange.Profiling.Storage;
-
-/// <summary>
-/// Provides saving and loading <see cref="MiniProfiler"/>s to a storage medium with some advanced operations.
-/// </summary>
-public interface IAdvancedAsyncStorage : IAsyncStorage
+namespace StackExchange.Profiling.Storage
 {
     /// <summary>
-    /// Asynchronously sets the provided profiler sessions to "viewed"
+    /// Provides saving and loading <see cref="MiniProfiler"/>s to a storage medium with some advanced operations.
     /// </summary>
-    /// <param name="user">The user to set this profiler ID as viewed for.</param>
-    /// <param name="ids">The profiler IDs to set viewed.</param>
-    Task SetViewedAsync(string user, IEnumerable<Guid> ids);
+    public interface IAdvancedAsyncStorage : IAsyncStorage
+    {
+        /// <summary>
+        /// Asynchronously sets the provided profiler sessions to "viewed"
+        /// </summary>
+        /// <param name="user">The user to set this profiler ID as viewed for.</param>
+        /// <param name="ids">The profiler IDs to set viewed.</param>
+        Task SetViewedAsync(string user, IEnumerable<Guid> ids);
+    }
 }
