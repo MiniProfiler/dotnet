95909b40dbed7d964400e363e5a4ceb647d892a7
Samo Prelog <samo@prelog.io>
2023-05-23 18:07:42 +0000

    added some tests

---
+++ tests/MiniProfiler.Tests/TimingInstrumentationTest.cs
@@ -0,0 +1,38 @@
+using System;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace StackExchange.Profiling.Tests
+{
+    public class TimingInstrumentationTest : BaseTest
+    {
+        public TimingInstrumentationTest(ITestOutputHelper output) : base(output) { }
+
+        private class TimingInstrumentation : IDisposable
+        {
+            public Timing Timing { get; set; }
+            public bool Disposed { get; set; }
+            public void Dispose() => Disposed = true;
+        }
+
+        [Fact]
+        public void IsInstrumented()
+        {
+            TimingInstrumentation instrumentation = null;
+            Timing timing = null;
+            Options.TimingInstrumentationProvider = t => instrumentation = new TimingInstrumentation { Timing = t };            
+            var mp = Options.StartProfiler();
+
+            using (timing = mp.Step("Test timing"))
+            {
+                Assert.NotNull(instrumentation);
+                Assert.False(instrumentation.Disposed);
+                mp.Increment();
+            }
+
+            Assert.NotNull(instrumentation);
+            Assert.Equal(timing, instrumentation.Timing);
+            Assert.True(instrumentation.Disposed);
+        }
+    }
+}
