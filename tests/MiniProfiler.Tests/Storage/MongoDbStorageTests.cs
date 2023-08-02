39bf8c868aa040fe78ff8f0e60839e8ab805fd9b
Ian Kemp <IanKemp@users.noreply.github.com>
2023-08-02 18:50:36 +0100

    Add new features to MongoDbStorage provider (#613)
    * New features:
        * Decimal fields can now be serialized as `NumberDecimal`s instead of raw strings.
        * Indexes can be automatically created instead of having to manually call the `WithIndexCreation` method.
        * MiniProfiler sessions can be configured to be automatically expired (deleted) after a certain time period has elapsed.
    * The `MongoDbStorageOptions` class has been added to allow for setting the above options.
    * MongoDB C# driver has been updated to the latest version, and obsoleted code elements updated accordingly.
    * All new features are opt-in; full backwards-compatibility (including binary) is retained.
    
    Co-authored-by: Ian Kemp <ian.kemp@capitalontap.com>
    Co-authored-by: Nick Craver <nrcraver@gmail.com>

--- tests/MiniProfiler.Tests/Storage/MongoDbStorageTests.cs
+++ tests/MiniProfiler.Tests/Storage/MongoDbStorageTests.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using MongoDB.Driver;
 using Xunit;
 using Xunit.Abstractions;
 
@@ -9,9 +10,37 @@ namespace StackExchange.Profiling.Tests.Storage
         public MongoDbStorageTests(MongoDbStorageFixture fixture, ITestOutputHelper output) : base(fixture, output)
         {
         }
+
+        [Fact]
+        public void RecreationHandling()
+        {
+            var options = new MongoDbStorageOptions
+            {
+                ConnectionString = TestConfig.Current.MongoDbConnectionString,
+                CollectionName = "MPTest" + Guid.NewGuid().ToString("N").Substring(20),
+            };
+
+            var storage = new MongoDbStorage(options);
+            Assert.NotNull(storage);
+            // Same options, won't throw
+            var storage2 = new MongoDbStorage(options);
+            Assert.NotNull(storage2);
+
+            options.CacheDuration = TimeSpan.FromSeconds(20);
+
+            // MongoDB.Driver.MongoCommandException : Command createIndexes failed: Index with name: Started_1 already exists with different options/An equivalent index already exists with the same name but different options.
+            var ex = Assert.Throws<MongoCommandException>(() => new MongoDbStorage(options));
+            Assert.NotNull(ex);
+            Assert.Equal(85, ex.Code);
+
+            options.AutomaticallyRecreateIndexes = true;
+            // Succeeds, because drop/re-create is allowed now
+            var storage4 = new MongoDbStorage(options);
+            Assert.NotNull(storage4);
+        }
     }
 
-    public class MongoDbStorageFixture : StorageFixtureBase<MongoDbStorage>, IDisposable
+    public class MongoDbStorageFixture : StorageFixtureBase<MongoDbStorage>
     {
         public MongoDbStorageFixture()
         {
